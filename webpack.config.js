423e804d5f28bd4d83cca291b6d90710b63fc60f
Vincent Dusautoir <ventinc@Vincents-MacBook-Pro.local>
2020-02-23 10:47:25 +0100

    Feat: Update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,20 @@ module.exports = {
     filename: 'andeor.js',
     library: 'Andeor',
     libraryTarget: 'umd',
+    libraryExport: 'default',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
